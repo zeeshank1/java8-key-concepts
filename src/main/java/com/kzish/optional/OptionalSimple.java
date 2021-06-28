3adfa93eb9f70870cca2f4e1ff67a36a1919fa9c
Zeeshan Khan <zeeshank1@gmail.com>
2021-06-29 01:21:43 +0530

    First commit

---
+++ src/main/java/com/kzish/optional/OptionalSimple.java
@@ -0,0 +1,19 @@
+package com.kzish.optional;
+
+import java.util.Optional;
+
+public class OptionalSimple {
+
+    public static void main(String[] args) {
+        Optional<String> vehicle = Optional.of("CAR");
+        String answerOne = "YES";
+        String answerTwo = null;
+
+        System.out.println("Non-empty optional   : " + vehicle);
+        System.out.println("Non-empty optional 1 : " + vehicle.get());
+        System.out.println("Empty check          : " + vehicle.orElse("NONE"));
+        System.out.println("Without null         : " + Optional.of(answerOne));
+        System.out.println("Of Nullable          : " + Optional.ofNullable(answerTwo));
+        System.out.println("Without null 2       : " + Optional.of(answerTwo));
+    }
+}
