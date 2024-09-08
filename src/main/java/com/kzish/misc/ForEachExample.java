d6033d99fdb6727d5b2dc645ba222f33e7d991d6
Zeeshan Khan <zeeshank1@gmail.com>
2024-09-08 23:59:07 +0530

    ForEach Loop with sysout

---
+++ src/main/java/com/kzish/misc/ForEachExample.java
@@ -0,0 +1,16 @@
+package com.kzish.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachExample {
+    public static void main(final String... args) {
+        List<Integer> integerList = new ArrayList<>();
+
+        for (int index = 0; index < 10; index++) {
+            integerList.add(index);
+        }
+
+        integerList.forEach(System.out::print);
+    }
+}
