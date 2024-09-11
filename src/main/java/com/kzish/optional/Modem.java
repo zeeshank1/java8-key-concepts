7de7abda1c6ef9a99a41adabe534e8f44d5c84c1
Zeeshan Khan <zeeshank1@gmail.com>
2024-09-11 23:59:52 +0530

    new changes

---
+++ src/main/java/com/kzish/optional/Modem.java
@@ -0,0 +1,19 @@
+package com.kzish.optional;
+
+public class Modem {
+
+    private Double price;
+
+    public Modem(final Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(final Double price) {
+        this.price = price;
+    }
+}
+
