166b6bb46b929d1163b5cd30a3736321fda6216d
Zeeshan Khan <zeeshank1@gmail.com>
2025-01-01 23:40:15 +0530

    Create LambdaAlpha.java

---
+++ src/main/java/com/kzish/LambdaAlpha.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class IterateExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        //without Lambda
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        //with Lambda
+    }
+}
