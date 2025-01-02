fffc516b2cf66a9cb48d04754f51a8077ea60e4a
Zeeshan Khan <zeeshank1@gmail.com>
2025-01-02 23:49:01 +0530

    Update ForEachExample.java

--- src/main/java/com/kzish/misc/ForEachExample.java
+++ src/main/java/com/kzish/misc/ForEachExample.java
@@ -12,5 +12,14 @@ public class ForEachExample {
         }
 
         integerList.forEach(System.out::print);
+
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        names.forEach(name -> System.out.println(name));
     }
 }
